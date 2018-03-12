254301f489a8c786982bf082d8769fcdffd5e284
Brian Caterfino <bcaterfino@gmail.com>
2018-03-12 23:15:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById("nested").querySelector(".target");
+
+}
+
+function increaseRankBy(n) {
+  var a =document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for(let i=0;i<a.length;i++) {
+    a[i].innerHTML=parseInt(a[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+    var current = document.getElementById('grand-node')
+    while (current.firstElementChild) {
+      current = current.firstElementChild
+    }
+    return current
+}

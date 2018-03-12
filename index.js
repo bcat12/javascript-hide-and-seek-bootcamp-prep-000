7334508984bab886ac29e2fdbff596b5013e050d
Brian Caterfino <bcaterfino@gmail.com>
2018-03-12 22:43:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(){
- return document.getElementById('nested').querySelector('target')
+ return document.getElementById('nested').querySelector('div.target')
 }
 
 function increaseRankBy(n){
